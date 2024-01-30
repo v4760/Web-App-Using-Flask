a5607335a3bc1969c185081ba6f14c3cdf1ee48c
Vikas Kagawad <vikaskagawad@gmail.com>
2024-01-30 09:33:30 -0500

    Add files via upload

---
+++ flaskblog/routes.py
@@ -0,0 +1,48 @@
+
+from flask import render_template, url_for, flash,redirect
+from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.models import User, Post
+from flaskblog import app,db, bcrypt
+
+
+posts = [
+    {
+        'author': 'Vikas Kagawad',
+        'title':'My Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20 2018'
+    },
+    {
+        'author': 'Vinay Kagawad',
+        'title':'My Post 2',
+        'content': 'Second post content',
+        'date_posted': 'December 10 2018'
+    }
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html',posts=posts, title= 'About')
+
+@app.route("/register", methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username= form.username.data, email = form.email.data, password= hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Your account has been created.You can login now','success')
+        return redirect(url_for('login'))
+
+    return render_template('register.html',title='Register',form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html',title='Login',form=form)
